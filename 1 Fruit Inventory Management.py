903b12a1489ed5013bed3e541685e8673d42f371
hrithikkhatik <hrithikkhatik360@gmail.com>
2025-12-11 21:14:32 +0530

    Add files via upload

---
+++ 1 Fruit Inventory Management.py
@@ -0,0 +1,13 @@
+t1 = ("mango", "orange","apple")
+l1 = list(t1)
+l1.append("banana")
+l1.append("grapes")
+l1.remove("orange")
+t2 = tuple(l1)
+print(len(t2))
+print(t2)
+print(t2[0])
+print(t2[-1])
+
+
+
